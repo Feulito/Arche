4dc77b30e6ccb7d55886847a6da7102a88eb4d32
Sebastien Duterte <sebastienduterte@hotmail.fr>
2021-08-07 15:21:31 +0200

    correctif using

--- Web/Controllers/IndexController.cs
+++ Web/Controllers/IndexController.cs
@@ -1,14 +1,13 @@
 ﻿using Core.Entities.Site;
+using Core.Models;
 using Core.Models.ViewModels;
 using Core.Services.Interfaces;
 using Core.Utils;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Web.Helpers;
 using Web.Models.Site;
 
 namespace Web.Controllers
@@ -16,29 +15,23 @@ namespace Web.Controllers
     public class IndexController : Controller
     {
         private readonly IArticleService _articleService;
-        private readonly IUserService _userService;
 
         private static readonly int NbArticles = 3;
 
-        public IndexController(IArticleService articleService, IUserService userService)
+        public IndexController(IArticleService articleService)
         {
             _articleService = articleService;
-            _userService = userService;
         }
 
-        [AllowAnonymous]
         public async Task<IActionResult> Index()
         {
-            await DataInitializer.Initialize(false);
-            UserViewModel user = User.Identity.IsAuthenticated ? await ConnectionHelper.GetRights(User, _userService) : null;
+            await DataInitializer.Initialize(true);
 
             List<ArticleViewModel> articles = MapperUtility.Map<Article, ArticleViewModel>((await _articleService.GetArticles(NbArticles)).OrderBy(a => a.Creation));
             IndexViewModel viewModel = new IndexViewModel() {
-                Articles = articles,
-                User = user
+                Articles = articles
             };
             return View(viewModel);
         }
-
     }
 }
